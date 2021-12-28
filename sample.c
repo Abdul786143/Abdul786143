257d5aea521335a4109694a7963f0447c182efbd
Abdul786143 <96772833+Abdul786143@users.noreply.github.com>
2021-12-28 15:49:51 +0530

    c language

---
+++ sample.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	printf("introduce my self");
+	printf("my name is jayakumar");
+    printf("iam from tirupathi");
+    printf("local");
+    printf("i got seat in svce");
+    printf ("mechanical branch");
+    printf("my dream ");
+    printf("i do anything");
+}
